f09b126411d5119e675511f65e0860d5c999d761
amok6from7 <kwifi7@gmail.com>
2020-03-25 22:26:12 +0900

    read csv and write

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,29 @@ extern crate csv;
 
 use std::env;
 use std::error::Error;
-use std::ffi::OsString;
+use std::io;
 use std::process;
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let file_path = get_first_arg()?;
-    let mut wtr = csv::Writer::from_path(file_path)?;
-    wtr.write_record(&["City", "State", "Population", "Latitude", "Longitude"])?;
-    wtr.write_record(&["Davidsons Landing", "AK", "", "65.2419444", "-165.2716667"])?;
-    wtr.write_record(&["Kenai", "AK", "7610", "60.5544444", "-151.2583333"])?;
-    wtr.write_record(&["Oakman", "AL", "", "33.7133333", "-87.3886111"])?;
+    let query = match env::args().nth(1) {
+        None => return Err(From::from("expected 1 argument, but got none")),
+        Some(query) => query,
+    };
+
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+
+    wtr.write_record(rdr.headers()?)?;
+
+    for result in rdr.records() {
+        let record = result?;
+        if record.iter().any(|field| field == &query) {
+            wtr.write_record(&record)?;
+        }
+    }
+
     wtr.flush()?;
     Ok(())
-}
-
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
 }
 
 fn main() {
